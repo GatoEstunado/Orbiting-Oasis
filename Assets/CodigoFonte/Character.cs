b2ada1a56410cd2bbd67c50d3680e2b2e07ea7ff
aluno <aluno@A2721>
2023-10-20 17:15:08 -0300

    versao1.0

---
+++ Assets/CodigoFonte/Character.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour
+{
+
+    public Sprite[] characterPoses = null;
+
+    // Use this for initialization 
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame 
+    void Update()
+    {
+
+    }
+}
