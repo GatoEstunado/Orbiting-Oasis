7cf4cda724ba8c4983cb5fef1f371f7e662c9a42
FG55510 <fernanda.ferreira.ismart@gmail.com>
2023-10-30 18:14:27 -0300

    Codigos que irão controlar as escolhas

---
+++ Assets/CodigoFonte/ChooseController.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChooseController : MonoBehaviour
+{
+    public ChooseLabelController label;
+    public GameController gameController;
+    private RectTransform rectTransform;
+    private float labelHeight = -1;
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void SetupChoose(ChooseScene scene)
+    {
+        DestroyLabels();
+        for (int index = 0; index < scene.labels.Count; index++)
+        {
+            ChooseLabelController newLabel = Instantiate(label.gameObject, transform).GetComponent<ChooseLabelController>();
+
+            if (labelHeight == -1)
+            {
+                labelHeight = newLabel.GetHeight();
+            }
+
+            newLabel.Setup(scene.labels[index], this, CalculateLabelPosition(index, scene.labels.Count));
+        }
+
+        Vector2 size = rectTransform.sizeDelta;
+        size.y = (scene.labels.Count + 2) * labelHeight;
+        rectTransform.sizeDelta = size;
+    }
+
+    public void PerformChoose(StoryScene scene)
+    {
+        gameController.PlayScene(scene);
+    }
+
+    private float CalculateLabelPosition(int labelIndex, int labelCount) //Organiza os botões de escolha
+    {
+        if (labelCount % 2 == 0)//numero par
+        {
+            if (labelIndex < labelCount / 2)
+            {
+                return labelHeight * (labelCount / 2 - labelIndex - 1) + labelHeight / 2;
+            }
+            else
+            {
+                return -1 * (labelHeight * (labelIndex - labelCount / 2) + labelHeight / 2);
+            }
+        }
+        else
+        {
+            if (labelIndex < labelCount / 2) //numero impar
+            {
+                return labelHeight * (labelCount / 2 - labelIndex);
+            }
+            else if (labelIndex > labelCount / 2)
+            {
+                return -1 * (labelHeight * (labelIndex - labelCount / 2));
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+
+    private void DestroyLabels() //destruira os botoes quando necessario
+    {
+        foreach (Transform childTransform in transform)
+        {
+            Destroy(childTransform.gameObject);
+        }
+    }
+}
