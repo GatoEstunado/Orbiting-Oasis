77b7420bafcbd6b352ef94a839e20839881afae9
FG55510 <fernanda.ferreira.ismart@gmail.com>
2023-10-30 17:58:13 -0300

    gamecontroller ver 1.2

--- Assets/CodigoFonte/GameController.cs
+++ Assets/CodigoFonte/GameController.cs
@@ -7,12 +7,25 @@ public class GameController : MonoBehaviour
     public StoryScene currentScene;
     public BottomBarController bottomBar;
     public BackgroundController backgroundController;
+    public ChooseController chooseController;
+
+    private State state = State.IDLE;
+
+    private enum State
+    {
+        IDLE, ANIMATE, CHOOSE
+    }
 
     void Start()
     {
-        bottomBar.PlayScene(currentScene); //da play na primera cena
-        backgroundController.SetImage(currentScene.background);//muda background
+        if (currentScene is StoryScene)
+        {
+            StoryScene storyScene = currentScene as StoryScene;
+            bottomBar.PlayScene(currentScene); //da play na primera cena
+            backgroundController.SetImage(currentScene.background);//muda background
+        }
     }
+
 
     void Update()
     {
@@ -31,6 +44,36 @@ public class GameController : MonoBehaviour
                     bottomBar.PlayNextSentence(); //proxima frase
                 }
             }
+        }
+    }
+
+
+
+    public void PlayScene(GameScene scene)
+    {
+        StartCoroutine(SwitchScene(scene));
+    }
+    private IEnumerator SwitchScene(GameScene scene)
+    {
+        state = State.ANIMATE;
+        currentScene = scene;
+        bottomBar.Hide();
+        yield return new WaitForSeconds(1f);
+        if (scene is StoryScene)
+        {
+            StoryScene storyScene = scene as StoryScene;
+            backgroundController.SetImage(storyScene.background);
+            yield return new WaitForSeconds(1f);
+            bottomBar.ClearText();
+            bottomBar.Show();
+            yield return new WaitForSeconds(1f);
+            bottomBar.PlayScene(storyScene);
+            state = State.IDLE;
+        }
+        else if (scene is ChooseScene)
+        {
+            state = State.CHOOSE;
+            chooseController.SetupChoose(scene as ChooseScene);
         }
     }
 }
